423e2d06d9bc3b6457c06e48e694fcca00e3dfb2
Chanwoo Nam <chanwoo.1993n@gmail.com>
2019-11-25 11:19:35 +0900

    191125 | heroku

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rjf4f%_@-jym%8j9ajq5-zc2-$w--!c_)#*se9dn)@i301dm%g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -143,3 +144,9 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # 로그인 후 리다이렉트 경로
 LOGIN_REDIRECT_URL = 'photos:index'
+
+
+# heroku settings
+import django_heroku
+django_heroku(locals())
+
